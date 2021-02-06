0035330a337fc08d40b467e0cb2ef2dc5c99b20e
Francisco Vásquez <Francisco.recancoj@Galileo.edu>
2021-02-06 13:13:49 -0600

    Mi primer repositorio Galileo

---
+++ Tarea2.js
@@ -0,0 +1,11 @@
+var mes = parseInt(prompt("¿Que estacion del año es, ingresa numero de mes?"))
+
+if(mes >= 1 && mes <= 2){
+  alert("Este mes " + "es " + "Invierno")
+  alert("21004274 Francisco Gabriel Vásquez Recancoj")
+}else if (mes == 12){
+  alert("Este mes " + "es " + "Invierno")
+  alert("21004274 Francisco Gabriel Vásquez Recancoj")
+}else if (mes >= 6 && mes <= 8){
+  alert("Este mes " + "es " + "Verano")
+  alert("21004274 Francisco Gabriel Vásquez Recancoj")
